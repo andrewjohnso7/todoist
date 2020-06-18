17902617ab376770930c96bbecc0f6b0422ea1b8
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-06-17 19:19:21 -0700

    additional init styling for Categories

--- app/containers/MainMenu/styles.js
+++ app/containers/MainMenu/styles.js
@@ -2,21 +2,34 @@ import styled from 'styled-components';
 
 export const PrimaryView = styled.main`
   height: 100vh;
-  background: #f4f4f4;
+  background: #d5d3d7;
   font-family: 'Neue Haas';
   font-weight: 800;
   font-style: bold;
+  border: 1px solid red;
 `;
 
 export const Title = styled.h3`
-  font-weight: 800;
-  font-style: bold;
+  text-align: center;
+  font-weight: 600;
+  font-style: normal;
+  color: #8a8a8d;
+  text-transform: uppercase;
+  border: 1px solid #8a8a8d;
+  width: 250px;
+  margin: 25px;
+  border-left: none;
+  border-right: none;
+  padding-top: 4px;
+  padding-bottom: 2px;
+  letter-spacing: 5px;
 `;
 
 export const Category = styled.div`
   font-family: 'Neue Haas';
   font-weight: 200;
   font-style: normal;
+  border: 1px solid pink;
 `;
 
 export const CategoryTwo = styled.div`
